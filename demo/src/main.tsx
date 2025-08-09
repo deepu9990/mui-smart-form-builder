697f697cc2b85731097b7a675c16ce16eb0b5561
Deepak Sharma <dsadeepak29@gmail.com>
2025-08-09 23:24:38 +0530

    chore: update date adapter to Day.js, remove unused dependencies, and clean up package-lock

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 const theme = createTheme({
   palette: {
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
         <App />
       </LocalizationProvider>
     </ThemeProvider>
